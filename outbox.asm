f083e8f3a7b83bb3df4e42896b97f251e04952bb
honey data <honeydatax@sapo.pt>
2016-09-17 20:05:17 +0000

    free code assembly

---
+++ outbox.asm
@@ -0,0 +1,39 @@
+; programa to exit out of dosbox on  use in int 19h
+
+push offset MSG
+call print
+
+call waitkey
+
+call outbox
+
+call exit
+
+proc outbox
+XOR AX,AX
+int 19h
+endproc
+
+
+proc waitkey
+
+XOR ah,ah
+int 16h
+
+endproc
+
+
+proc print(word address)
+mov DX,address
+mov ah,9
+int 21h
+endproc 
+
+
+
+proc exit
+
+int 20h
+endproc
+
+MSG dB "press a key to exit$"
